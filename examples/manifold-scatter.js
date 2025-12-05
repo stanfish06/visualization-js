9a32199791c4841bb6dfe56d83ab42ff12619630
Stan <zyyu@umich.edu>
2025-12-05 03:33:23 -0500

    fix axes

--- examples/manifold-scatter.js
+++ examples/manifold-scatter.js
@@ -1,6 +1,15 @@
 import * as d3 from "d3";
 import { D3DOM } from "../lib.js";
+// create container
+const scatter = new D3DOM();
 
+// svg parameters
+const margin = {top: 20, right: 20, bottom: 50, left: 50}
+const width = 800 + margin.left + margin.right;
+const height = 800 + margin.top + margin.bottom;
+scatter.createSVG(width, height);
+
+// data
 const data = (() => {
   const random = d3.randomNormal(0, 1);
   return [].concat(
@@ -9,19 +18,34 @@ const data = (() => {
   );
 })();
 
-const x = d3.scaleLinear().domain([40, 55]).range([0, 800]);
+// themes
+const theme = `
+  svg { background: #1a1a1a; }
+  
+  .cluster-0 {
+    fill: #ff6b6b;
+    opacity: 0.7;
+  }
 
-const y = d3.scaleLinear().domain([40, 55]).range([800, 0]);
+  .cluster-1 {
+    fill: #ff6b6b;
+    opacity: 0.7;
+  }
+`
+scatter.updateStyle(theme)
 
-const scatter = new D3DOM();
-scatter.createSVG(1000, 1000);
+// axes
+const x = d3.scaleLinear().domain([40, 55]).range([margin.left, width - margin.right]);
+const y = d3.scaleLinear().domain([40, 55]).range([height - margin.bottom, margin.top]);
+
+// apply data to svg
 scatter.svg
   .append("g")
-  .attr("transform", "translate(0, 900)")
+  .attr("transform", `translate(0, ${height - margin.bottom})`)
   .call(d3.axisBottom(x));
 scatter.svg
   .append("g")
-  .attr("transform", "translate(50, 0)")
+  .attr("transform", `translate(${margin.left}, 0)`)
   .call(d3.axisLeft(y));
 scatter.svg
   .selectAll("circle")
@@ -31,5 +55,7 @@ scatter.svg
   .attr("cx", (d) => x(d[0]))
   .attr("cy", (d) => y(d[1]))
   .attr("r", 3)
-  .attr("fill", (d) => (d[2] === 0 ? "red" : "blue"));
+  .attr("class", (d) => (d[2] === 0 ? "cluster-0" : "cluster-1"));
+
+// export
 scatter.saveAsHTML("test.html");
